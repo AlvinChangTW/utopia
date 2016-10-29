75629e44fec3217c41946481f621fe5e9f999f0b
birdbrother <birdbrother@gmail.com>
2016-10-29 23:54:45 +0800

    STI form_for droplist try success

--- app/controllers/goods_controller.rb
+++ app/controllers/goods_controller.rb
@@ -2,4 +2,20 @@ class GoodsController < ApplicationController
   def index
     @goods = Good.all.page(params[:page]).per(10)
   end
+
+  def new
+    @good = Good.new
+  end
+
+  def create
+    @good = Good.new(good_params)
+    @good.save
+    redirect_to goods_path
+  end
+
+  private
+  def good_params
+    params.require(:good).permit(:name, :description, :area, :type, :category_id)
+  end
+
 end
