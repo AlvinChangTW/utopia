a88368beb87dcce7ab94317b776266b6c9d4bb58
birdbrother <birdbrother@gmail.com>
2016-11-01 22:38:16 +0800

    fix migrate

--- db/migrate/20161030103730_add_column_user_id_to_good.rb
+++ db/migrate/20161030103730_add_column_user_id_to_good.rb
@@ -1,6 +1,6 @@
 class AddColumnUserIdToGood < ActiveRecord::Migration[5.0]
   def change
-    add_column :goods, :user_id, :interger
+    add_column :goods, :user_id, :integer
     add_index :goods, :user_id
   end
 end
