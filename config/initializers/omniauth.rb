2a3bee6c6c71c9f7afc869a43c11e941e4da4e7b
birdbrother <birdbrother@gmail.com>
2016-11-02 21:43:26 +0800

    fix fb login

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,6 +1,6 @@
 OmniAuth.config.logger = Rails.logger
 Rails.application.config.middleware.use OmniAuth::Builder do
   fb_config = Rails.application.config_for(:facebook)
-  provider :facebook, fb_config['app_id'], fb_config['token']
+  provider :facebook, fb_config['app_id'], fb_config['secret']
 
 end
