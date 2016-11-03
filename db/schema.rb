80ffc3836a4e1b918f0401d2b2bcb58ca4277e9c
birdbrother <birdbrother@gmail.com>
2016-11-03 13:10:00 +0800

    change local db to mysql

--- db/schema.rb
+++ db/schema.rb
@@ -12,40 +12,42 @@
 
 ActiveRecord::Schema.define(version: 20161031113929) do
 
-  create_table "categories", force: :cascade do |t|
+  create_table "categories", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4" do |t|
     t.string   "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
-  create_table "comments", force: :cascade do |t|
-    t.text     "content",    null: false
-    t.integer  "user_id",    null: false
-    t.integer  "good_id",    null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.         "secret"
-    t.index ["good_id"], name: "index_comments_on_good_id"
-    t.index ["user_id"], name: "index_comments_on_user_id"
+  create_table "comments", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4" do |t|
+    t.text     "content",    limit: 65535, null: false
+    t.integer  "user_id",                  null: false
+    t.integer  "good_id",                  null: false
+    t.datetime "created_at",               null: false
+    t.datetime "updated_at",               null: false
+    t.boolean  "secret"
+    t.index ["good_id"], name: "index_comments_on_good_id", using: :btree
+    t.index ["user_id"], name: "index_comments_on_user_id", using: :btree
   end
 
-  create_table "goods", force: :cascade do |t|
-    t.string   "name",                 null: false
+  create_table "goods", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4" do |t|
+    t.string   "name",                               null: false
     t.string   "area"
     t.string   "type"
     t.integer  "category_id"
     t.string   "status"
-    t.text     "description"
-    t.datetime "created_at",           null: false
-    t.datetime "updated_at",           null: false
+    t.text     "description",          limit: 65535
+    t.datetime "created_at",                         null: false
+    t.datetime "updated_at",                         null: false
     t.string   "picture_file_name"
     t.string   "picture_content_type"
     t.integer  "picture_file_size"
     t.datetime "picture_updated_at"
     t.integer  "user_id"
+    t.index ["category_id"], name: "index_goods_on_category_id", using: :btree
+    t.index ["user_id"], name: "index_goods_on_user_id", using: :btree
   end
 
-  create_table "users", force: :cascade do |t|
+  create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4" do |t|
     t.string   "name"
     t.string   "email"
     t.string   "fb_uid"
@@ -54,7 +56,7 @@ ActiveRecord::Schema.define(version: 20161031113929) do
     t.datetime "fb_expires_at"
     t.datetime "created_at",    null: false
     t.datetime "updated_at",    null: false
-    t.index ["fb_uid"], name: "index_users_on_fb_uid", unique: true
+    t.index ["fb_uid"], name: "index_users_on_fb_uid", unique: true, using: :btree
   end
 
 end
